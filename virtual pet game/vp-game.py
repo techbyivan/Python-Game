738facd1f4646f8d8a155db30d16137b76bd137a
Ivan Davis <techbyivan@users.noreply.github.com>
2020-06-10 15:21:04 -0400

    Created animal methods

--- virtual pet game/vp-game.py
+++ virtual pet game/vp-game.py
@@ -1,7 +1,35 @@
-import Petsss from animals
-
 class Pet:
-    def __init__(self, name, fullness=50, happiness=50):
+    def __init__(self, name, fullness=50, happiness=50, hunger=10, mopiness=10):
         self.name = name
         self.fullness = fullness
         self.happiness = happiness
+        self.discipline = discipline
+        self.hunger = hunger
+        self.mopiness = mopiness
+
+    def get_name(self):
+        self.name = ""
+
+    def eat_food(self):
+        self.fullness += 30
+
+    def get_love(self):
+        self.happiness += 30
+    
+    def get_disciplined(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness
+
+grayson = Pet("Grayson")
+grayson.eat_food()
+print(grayson.fullness)
+# 80
+print(grayson.happiness)
+# 50
+
+boog = Pet("Boog")
+boog.get_love()
+print(grayson.fullness)
+# 50
+print(boog.happiness)
+# 80

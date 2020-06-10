f85a113668c15d75d1ba76cf9bee0acbb2da1920
Ivan Davis <techbyivan@users.noreply.github.com>
2020-06-10 15:00:19 -0400

    Looping through animals

--- virtual pet game/animals.py
+++ virtual pet game/animals.py
@@ -1,38 +1,34 @@
+
+        
 cat =  '''
  C A T                
-
  /\_/\ \n           
 ( o.o ) \n          
  > ^ < \n           
-
-
 '''                   
-
 dog = '''
  D O G                                                             
   __      _ 
 o'')}____// 
  `_/      ) 
  (_(_/-(_/  
-
 '''
-
 bird = '''
  B I R D
  ,_
 >' )
 ( ( \ 
 ''|
-
 '''
 
-dog_1 = "Dog"
+
+dog_1 ="Dog"
 cat_2 = "Cat"
 bird_3 = "Bird"
 
 
 while True:
-    questions = input("Choose your favorite animal ? %s - , %s - , %s : " % (dog_1, cat_2, bird_3))
+    questions = input("Please choose your favorite animal ? %s - , %s - , %s : " % (dog_1, cat_2, bird_3))
     if questions == "Dog":
         print("\n Congratulations!! You have chosen a :\n")
         print(dog)
@@ -42,7 +38,10 @@ while True:
     elif questions == "Bird":
         print("\n Congratulations!! You have chosen a :\n")
         print(bird)
+    elif questions == "0":
+        print("\nYou have chosen to EXIT our program.\nThank you \n")
+        exit()
     else:
-        print("Please Chose your favorite animal")
+        print(questions)
     
 
---
+++ virtual pet game/vp-game.py
@@ -0,0 +1,7 @@
+import Petsss from animals
+
+class Pet:
+    def __init__(self, name, fullness=50, happiness=50):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
